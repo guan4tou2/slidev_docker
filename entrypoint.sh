fa4f1d3de7536fd2672fc470cb5beb3891815365
guantou <mole30608@gmail.com>
2023-04-29 16:21:57 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,10 +9,12 @@ if [ -f /slidev/slides.md ]; then
         npm install
     fi
     echo "Start slidev..."
+    sed -i '61d' node_modules/@slidev/client/internals/Editor.vue  #content.value = v
     npx slidev --remote=password
 else
     echo "slides.md not found in the bind mount to /slidev"
     npm install @slidev/cli @slidev/theme-default @slidev/theme-seriph
     cp -f /slidev/node_modules/@slidev/cli/template.md /slidev/slides.md
+    sed -i '61d' node_modules/@slidev/client/internals/Editor.vue  #content.value = v
     npx slidev --remote=password
 fi
